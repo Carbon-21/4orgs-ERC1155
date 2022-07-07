2be9edbc6991fb7642eb5022a71a9503ed858f7d
Pedro <pedro.barcha@tutanota.com>
2022-07-07 08:12:54 -0300

    scripts updated

--- api-2.0/util/validation.js
+++ api-2.0/util/validation.js
@@ -1,6 +1,6 @@
 const HttpError = require("./http-error");
 const { validationResult } = require("express-validator");
-const fs = require("fs");
+const logger = require("../util/logger");
 
 //throw error if data is not valid (express-validator)
 const validateAll = (req, res, next) => {
@@ -8,6 +8,10 @@ const validateAll = (req, res, next) => {
 
   //if there are errors
   if (!errors.isEmpty()) {
+    logger.debug("Req.jwt: " + JSON.stringify(req.jwt));
+    logger.debug("Req.params: " + JSON.stringify(req.params));
+    logger.debug("Req.query: " + JSON.stringify(req.query));
+    logger.debug("Req.body: " + JSON.stringify(req.body));
     throw new HttpError(422);
   }
 
