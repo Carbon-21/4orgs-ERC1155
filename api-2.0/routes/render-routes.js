51a96185d42b696f7ae3f3ad2dd6a89003e5ef71
HeyYukio <yukiomrt@gmail.com>
2022-07-18 20:57:54 -0300

    Tela provisoria de consulta nft e ft adicionadas e mudancas de bg

--- api-2.0/routes/render-routes.js
+++ api-2.0/routes/render-routes.js
@@ -2,12 +2,16 @@ const { Router } = require("express");
 
 const router = Router();
 
-router.get('/balance', (req, res)=>{
-  res.render('balance',{title: "Balance", cssPath: "css/balance.css" });
+router.get('/wallet', (req, res)=>{
+  res.render('wallet',{title: "My Wallet", cssPath: "css/wallet.css" });
 });
 
 router.get('/mint', function(req, res) {
   res.render('mint', {title: "Mint", cssPath: "css/mint.css"});
+});
+
+router.get('/collection', function(req, res) {
+  res.render('collection', {title: "My Collection", cssPath: "css/collection.css"});
 });
 
 router.get('/config', function(req, res) {
