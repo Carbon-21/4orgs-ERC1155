b6a5ec9e055cf1a4518c56b2dd714404773b187a
HeyYukio <yukiomrt@gmail.com>
2022-10-11 00:57:26 -0300

    mensagens de transferencia adicionadas

--- api-2.0/public/scripts/transfer.js
+++ api-2.0/public/scripts/transfer.js
@@ -34,14 +34,36 @@ async function transfer() {
 
   if (response.ok) {
     response = await response.json();
-    if (response.result == null) alert("Transferência Falhou");
+    if (response.result == null){
+      document.getElementById("submitButton").style.display = "flex";
+      document.getElementById("loader").style.display = "none";
+      element =     
+      `<div class="alert alert-danger alert-dismissible fade show mb-3 mt-3" role="alert">`+
+          `Ocorreu um erro na transferencia`+
+          `<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>`+
+      `</div>`
+      document.getElementById("flash").innerHTML = element;
+    }
     else {
       document.getElementById("submitButton").style.display = "flex";
       document.getElementById("loader").style.display = "none";
-      alert("Transferência realizada com sucesso");
+      element =     
+      `<div class="alert alert-success alert-dismissible fade show mb-3 mt-3" role="alert">`+
+          `Transferencia realizada com sucesso`+
+          `<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>`+
+      `</div>`
+    document.getElementById("flash").innerHTML = element;
     }
   } else {
     console.log("HTTP Error ", response.status);
+    document.getElementById("submitButton").style.display = "flex";
+    document.getElementById("loader").style.display = "none";
+    element =     
+    `<div class="alert alert-danger alert-dismissible fade show mb-3 mt-3" role="alert">`+
+        `Ocorreu um erro na transferencia`+
+        `<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>`+
+    `</div>`
+    document.getElementById("flash").innerHTML = element;
     return null;
   }
 }
