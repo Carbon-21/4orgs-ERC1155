5aa99bbc1565a80a6881695d87ee67660ca31ab6
Rafael Descio <desciot@gmail.com>
2023-02-16 11:40:34 -0500

    chore: Blockchain and database state persistence #closes #54

--- artifacts/channel/create-artifacts.sh
+++ artifacts/channel/create-artifacts.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-# Delete existing artifacts
-rm -rf genesis.block mychannel.tx
-rm -rf ../../channel-artifacts/*
 
 #Generate Crypto artifactes for organizations
 # cryptogen generate --config=./crypto-config.yaml --output=./crypto-config/
-
 
 
 # System channel
@@ -15,7 +11,7 @@ SYS_CHANNEL="sys-channel"
 # channel name defaults to "mychannel"
 CHANNEL_NAME="mychannel"
 
-echo $CHANNEL_NAME
+echo "Default Channel: $CHANNEL_NAME"
 
 # Generate System Genesis block
 configtxgen -profile OrdererGenesis -configPath . -channelID $SYS_CHANNEL  -outputBlock ./genesis.block
