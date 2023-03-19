4a9e24c5259f4a3ab99b866b524ce2c84d91999d
Emilly Arcanjo <emilly.ssa@gmail.com>
2023-03-19 18:48:18 -0300

    added marketplace's page

--- api-2.0/controllers/front-controller.js
+++ api-2.0/controllers/front-controller.js
@@ -235,6 +235,14 @@ exports.getCollection = async (req, res, next) => {
     });
 };
 
+///// MARKETPLACE CONTROLLERS /////
+
+exports.getMarketplace = async (req, res, next) => {
+  res.render("marketplace", {
+    title: "Marketplace",
+    cssPath: "css/collection.css",
+  });
+};
 
 ///// $ILVAS MINT CONTROLLERS /////
 
