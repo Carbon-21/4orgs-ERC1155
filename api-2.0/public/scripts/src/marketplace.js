3f0cef281d8caa5a21a2372088f901aab168bb5c
Emilly Arcanjo <emilly.ssa@gmail.com>
2023-04-02 19:26:29 -0300

    marketplace to show nft on sale

---
+++ api-2.0/public/scripts/src/marketplace.js
@@ -0,0 +1,200 @@
+let metadata;
+let metadataArray = [];
+
+async function marketplace() {
+  // Recuperar todos os nfts com status "sale"
+  let nftTokens = await getNftOnSale();
+  // Caso haja nfts
+  if (nftTokens) {
+    let element = '<div class="d-flex flex-column justify-content-between p-md-1">';
+    for (var key in nftTokens) {
+      let tokenId = nftTokens[key];
+      metadata = (await nftMetadata(tokenId))?.message;
+      metadataArray.push(metadata);
+      element +=
+        '<div class="card shadow-lg mt-3">' +
+        '<div class="card-body flex-column">' +
+        '<div class="d-flex justify-content-between p-md-1">' +
+        '<div class="d-flex flex-row">' +
+        '<div class="align-self-center">' +
+        '<i class="fa-solid fa-tree fa-4x tree-icon"></i>' +
+        "</div>" +
+        "<div>" +
+        `<button class="accordion-button" type="button" data-bs-toggle="collapse" aria-expanded="true" data-bs-target="#${tokenId.replace(
+          /\s/g,
+          ""
+        )}" aria-controls="${tokenId}"> ${tokenId} </button>` +
+        (await  renderMetadata(tokenId, metadata)) +
+        "</div>" +
+        "</div>" +
+        "</div>" +
+        "</div>" +
+        "</div>" +
+        "</div>";
+
+      // Renderizar a cada nft carregado
+      document.getElementById("nft-showroom").innerHTML = element;
+    }
+  } else {
+    console.log("HTTP Error ", response.status);
+    return null;
+  }
+
+  //Desabilitar gif do loader
+  document.getElementById("loader").style.display = "none";
+}
+
+// Recuperar todos os nfts com status "sale"
+async function getNftOnSale() {
+  let token = localStorage.getItem("token");
+  let headers = new Headers();
+  headers.append("Authorization", "Bearer " + token);
+  let url = `https://${HOST}:${PORT}/query/channels/mychannel/chaincodes/erc1155/CheckForStatus`;
+
+  var init = {
+    method: "GET",
+    headers: headers,
+  };
+ 
+  let response = await fetch(url, init)
+  let result = JSON.parse((await response.json())?.result);
+
+  let nftArray = [];
+  // Retornar array contendo somente a lista de ids dos nfts
+  for (var i in result) {
+    nftArray = nftArray.concat(result[i][1]);
+  }
+  
+  return nftArray;
+}
+
+// Recuperar json dos metadados do nft (dado tokenId)
+async function nftMetadata(tokenId) {
+  let token = localStorage.getItem("token");
+  let headers = new Headers();
+  headers.append("Content-Type", "application/json");
+  headers.append("Authorization", "Bearer " + token);
+
+  let url = `https://${HOST}:${PORT}/meta/getMetadata?tokenId=${tokenId}`;
+  var init = {
+    method: "GET",
+    headers: headers,
+  };
+
+  let response = await fetch(url, init);
+  return response.json();
+}
+
+// Retorna string com a construção dos metadados de dado nft (em div accordion colapsavel)
+async function renderMetadata(tokenId, metadata) {
+  if (!metadata.name) return "Metadados não recuperados";
+  return (
+    `<div id="${tokenId.replace(/\s/g, "")}" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample"> <div class="accordion-body">` +
+    "<p>" +
+    `<b> Status: </b> ${metadata?.properties?.status} <br />` +
+    // `<b> Quantidade: </b> ${metadata?.properties?.amount} <br />` +
+    `<b> Proprietário da Terra: </b> ${metadata?.properties?.land_owner} <br />` +
+    `<b> Área (hectares): </b> ${metadata?.properties?.land} <br />` +
+    `<b> Fitofisiologia: </b> ${metadata?.properties?.phyto} <br />` +
+    `<b> Geolocalização: </b> ${metadata?.properties?.geolocation} <br />` +
+    `<b> Dono dos direitos de Compensação: </b> ${metadata?.properties?.compensation_owner} <br />` +
+    renderCompensation(tokenId.replace(/\s/g, ""), metadata?.properties?.compensation_state) +
+    "<p>" +
+    "</div>"
+  );
+}
+
+// Retorna string do metadado de compensação, dependendo do estado
+function renderCompensation(tokenId, compensation_state) {
+  switch (compensation_state) {
+    case "Aguardando":
+      return `<b> Estado de compensação:</b> Aguardando <br />`;
+    case "Compensado":
+      return `<b> Estado de compensação:</b> Compensado <br />`;
+    // Inclui botão de compensação quando não compensado
+    case "Não Compensado":
+    default:
+      return (
+        `<b> Estado de compensação:</b> Não compensado <br />` +
+        `<button id="submitCompensationButton" type="submit" style="display: flex" class="btn btn-primary btn-md mt-3" onclick="compensate(${tokenId})">Compensar</button>`
+      );
+  }
+}
+
+//change token status to "Compensado" in the IPFS
+//OBS: funções de escrita e leitura dos metadados no IPFS foram feitas de maneira desiguais, deveriam receber/retornar mesma estrutura json. Por isso, apenas alguns campos são mantidos ao se compensar (ver variável body)
+async function compensate(tokenId) {
+  event.preventDefault();
+
+  //set loading
+  document.getElementById("loader").style.display = "flex";
+  document.getElementById("submitCompensationButton").style.display = "none";
+
+  tokenId = tokenId.id;
+
+  let jwt = localStorage.getItem("token");
+
+  let headers = new Headers();
+  headers.append("Content-Type", "application/json");
+  headers.append("Authorization", "Bearer " + jwt);
+  let url = `https://${HOST}:${PORT}/meta/patchMetadata`;
+
+  var init = {
+    method: "PATCH",
+    headers: headers,
+  };
+
+  //get token info
+  let tokenInfo = metadataArray.filter((metadataArray) => metadataArray.name === tokenId);
+  tokenInfo = tokenInfo[0].properties;
+
+  let body = {
+    tokenId,
+    metadata: {
+      ...tokenInfo,
+      compensation_state: "Compensado",
+    },
+  };
+
+  init.body = JSON.stringify(body);
+
+  //POST to postMetadata
+  let response = await fetch(url, init);
+  // let element =
+  //   `<div class="alert alert-danger alert-dismissible fade show mb-3 mt-3" role="alert">` +
+  //   `Compensando...` +
+  //   `<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>` +
+  //   `</div>`;
+  // document.getElementById("flash").innerHTML = element;
+
+  if (response.ok) {
+    document.getElementById("loader").style.display = "none";
+    response = await response.json();
+    if (response.result != "success") {
+      let element =
+        `<div class="alert alert-danger alert-dismissible fade show mb-3 mt-3" role="alert">` +
+        `Ocorreu um erro na compensação` +
+        `<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>` +
+        `</div>`;
+      document.getElementById("flash").innerHTML = element;
+    } else {
+      let element =
+        `<div class="alert alert-success alert-dismissible fade show mb-3 mt-3" role="alert">` +
+        `Compensado com sucesso` +
+        `<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>` +
+        `</div>`;
+      document.getElementById("flash").innerHTML = element;
+    }
+    window.location.href = `/collection`;
+  } else {
+    document.getElementById("loader").style.display = "none";
+    console.log("HTTP Error ", response.status);
+    let element =
+      `<div class="alert alert-danger alert-dismissible fade show mb-3 mt-3" role="alert">` +
+      `Ocorreu um erro na compensação` +
+      `<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>` +
+      `</div>`;
+    document.getElementById("flash").innerHTML = element;
+    return null;
+  }
+}
