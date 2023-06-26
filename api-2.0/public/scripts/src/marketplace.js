8a9bf0ce759aa2cddca5cbdb7db0d71f837f84f2
Emilly Arcanjo <emilly.ssa@gmail.com>
2023-06-26 03:42:51 -0300

    update metadata and add pop up to buy

--- api-2.0/public/scripts/src/marketplace.js
+++ api-2.0/public/scripts/src/marketplace.js
@@ -6,84 +6,112 @@ async function marketplace() {
   document.getElementById("loader").style.display = "flex";
   
   // Recuperar todos os nfts com status "sale"
-  let nftTokens = await getNftOnSale();
-  let nftTokensGeneral = await getNftTokens();
+  let nftPrice = await getNftOnSalePrice();
+  let nftMetadata = await getNftOnSaleMetadata();
+
   // Caso haja nfts
-  if (nftTokens) {
+  if (nftMetadata && nftPrice) {
     let element = '<div class="d-flex flex-column justify-content-between p-md-1">';
-    if (nftTokens.length == 0){
-      element +=
-        '<center><h2><font color="#5f5f5f">Não existem NFTs à venda </font></h2> </center>'+
-        "</div>";
-        document.getElementById("nft-showroom").innerHTML = element;        
-    }
-    else{
-      for (var index in nftTokens) {
-        let nftinfo = "";
+    for (var index in nftPrice) {
+      let nftinfo = "";
 
-        let tokenId = nftTokens[index].id;
-        let priceWithTaxes = parseInt(nftTokens[index].price) + parseInt(nftTokens[index].taxes);
+      let tokenId = nftPrice[index].id;
+      let price =  parseInt(nftPrice[index].price);
+      let taxes =  parseInt(nftPrice[index].taxes);
+      let priceWithTaxes = price + taxes;
 
-        for (var key in nftTokensGeneral) {
-          if (nftTokens[index].id == nftTokensGeneral[key][0]){  
-            nftinfo = JSON.parse(nftTokensGeneral[key][1]);
-          }
+      for (var key in nftMetadata) {
+        if (nftPrice[index].id == nftMetadata[key][0]){  
+          nftinfo = JSON.parse(nftMetadata[key][1]);
         }
+      }
 
-        element +=
-          '<div class="card shadow-lg mt-3">' +
-            '<div class="card-body flex-column">' +
-              '<div class="d-flex justify-content-between p-md-1">' +
-                '<div class="d-flex flex-row">' +
-                  '<div class="align-self-center">' +
-                    '<i class="fa-solid fa-tree fa-4x tree-icon"></i>' +
-                  "</div>" +
-                  "<div>" +
-                    //`<button class="accordion-button cursor-pointer" type="button" data-bs-toggle="collapse" aria-expanded="true" data-bs-target='#tk${tokenId.replace(/\s/g,"")}' aria-controls="tk${tokenId}"> 
-                    //    <p>
-                    //      ${tokenId.slice(1)} <br /></p>                                          
-                    //</button>` +
-                     `<button class="accordion-button cursor-pointer" type="button" data-bs-toggle="collapse" aria-expanded="true" data-bs-target='#tk${tokenId.replace(/\s/g,"")}' aria-controls="tk${tokenId}"> 
-                         <p>
-                           ${tokenId.slice(1)} <br />
-                           <b> Área (hectares): </b> ${nftinfo?.metadata?.land} <br />
-                           <b> Fitofisiologia: </b> ${nftinfo?.metadata?.phyto} <br /> 
-                           <b> Geolocalização: </b> ${nftinfo?.metadata?.geolocation} <br />  
-                         </p>                                          
-                     </button>` +
-                    '<div class="d-flex flex-row gap-2">' +
-                      // `<button id="seeMoreButton" class="btn btn-primary btn-md" type="button" data-bs-toggle="collapse" aria-expanded="true" data-bs-target='#tk${tokenId.replace(/\s/g,"")}' aria-controls="tk${tokenId}" onclick="seeMoreButton()"> 
-                      //   Ver mais                                       
-                      // </button>` +
-                      `<button id="buyButton" type="button" class="btn btn-primary btn-md" onclick='buy("${tokenId}")' > 
-                        Comprar 
-                      </button>`+
-                    '</div>'+        
-                    //(await  renderMetadata(tokenId, nftinfo)) +
-                  "</div>" +
+      element +=
+        '<div class="card shadow-lg mt-3">' +
+          '<div class="card-body flex-column">' +
+            '<div class="d-flex justify-content-between p-md-1">' +
+              '<div class="d-flex flex-row">' +
+                '<div class="align-self-center">' +
+                  '<i class="fa-solid fa-tree fa-4x tree-icon"></i>' +
                 "</div>" +
-                '<div class="d-flex flex-row">' +
-                  '<div class="align-self-center" style="margin-right: 30px">' +
-                    '<i class="fa-solid fa-coins fa-4x coin-icon"></i>'+
-                  "</div>" +
-                  '<div class="align-self-center">' +
-                    `<h2 id="balanceHeader" class="h1 mb-0">${priceWithTaxes} C21 </h2>` +
-                  '</div>' +
-                '</div>' +
+                "<div>" +
+                    `<button class="accordion-button cursor-pointer" type="button" data-bs-toggle="collapse" aria-expanded="true" data-bs-target='#tk${tokenId.replace(/\s/g,"")}' aria-controls="tk${tokenId}"> 
+                        <p>
+                          <b> ID: </b>${tokenId.slice(1)} <br /> 
+                          <b> Área (hectares): </b> ${nftinfo?.metadata?.land_area} <br />
+                          <b> Fitofisiologia: </b> ${nftinfo?.metadata?.phyto} <br /> 
+                          <b> Geolocalização: </b> ${nftinfo?.metadata?.geolocation} <br />  
+                        </p>                                          
+                    </button>` +
+                  '<div class="d-flex flex-row gap-2">' +
+                    `<button id="seeMoreButton" class="btn btn-primary btn-md" type="button" data-bs-toggle="collapse" aria-expanded="true" data-bs-target='#tk${tokenId.replace(/\s/g,"")}' aria-controls="tk${tokenId}" onclick="seeMoreButton()"> 
+                      Ver mais                                       
+                    </button>` +
+                    `<button id="buyButton" type="button" class="btn btn-primary btn-md" data-bs-toggle="modal" data-bs-target="#confirmation"> 
+                      Comprar 
+                    </button>`+
+
+                  '</div>'+        
+                  (await  renderMetadata(tokenId, nftinfo)) +
+                "</div>" +
               "</div>" +
+              '<div class="d-flex flex-row">' +
+                '<div class="align-self-center" style="margin-right: 30px">' +
+                  '<i class="fa-solid fa-coins fa-4x coin-icon"></i>'+
+                "</div>" +
+                '<div class="align-self-center">' +
+                  `<h2 id="balanceHeader" class="h1 mb-0">${priceWithTaxes} C21 </h2>` +
+                '</div>' +
+              '</div>' +
             "</div>" +
           "</div>" +
-        "</div>";
-      
-        // Renderizar a cada nft carregado
-        document.getElementById("nft-showroom").innerHTML = element;
-        //Habilita card, pois algumas opções o desabilitam 
-        document.getElementById("nft-showroom").style.display = "block";
-      }
+        "</div>" +
+      "</div>"+
+
+      `<div class="modal fade" id="confirmation" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="staticBackdropLabel">Confirmação de compra</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <div class="d-flex flex-row"> 
+                <div class="align-self-center" style="margin-right: 30px"> 
+                  <i class="fa-solid fa-coins fa-4x coin-icon"></i>
+                </div> 
+                <div class="align-self-center">
+                  <b> Preço: </b> ${price} C21<br />
+                  <b> Taxa: </b> ${taxes} C21 <br /> 
+                  <b> Total: </b> ${priceWithTaxes} C21<br />
+                </div>
+              </div> 
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+              <button id="comprar" type="button" class="btn btn-primary" onclick='buy("${tokenId}")'>Confirmar</button>
+            </div>
+          </div>
+        </div>
+      </div>`;
+    
+      // Renderizar a cada nft carregado
+      document.getElementById("nft-showroom").innerHTML = element;
+      //Habilita card, pois algumas opções o desabilitam 
+      document.getElementById("nft-showroom").style.display = "block";
     }
   }
   else {
-    console.log("HTTP Error ", response.status);
+    //se nenhum dos dois encontrar, é porque não tem, caso contrário, ocorreu algum erro
+    if (nftMetadata.length()==0 && nftPrice.length()==0) {
+      element +=
+        '<center><h2><font color="#5f5f5f">Não existem NFTs à venda </font></h2> </center>'+
+        "</div>";
+        document.getElementById("nft-showroom").innerHTML = element;   
+    }     
+    else{ 
+      console.log("HTTP Error ", response.status);
+    }
     return null;
   }
   
@@ -92,8 +120,8 @@ async function marketplace() {
   document.getElementById("loader").style.display = "none";
 }
 
-// Recuperar todos os nfts com status "sale"
-async function getNftOnSale() {
+// Recuperar o preço e a taxa de todos os nfts com status "sale"
+async function getNftOnSalePrice() {
   let token = localStorage.getItem("token");
   let headers = new Headers();
   headers.append("Authorization", "Bearer " + token);
@@ -128,21 +156,30 @@ async function getNftOnSale() {
   return nftArray;
 }
 
-// Recuperar json dos metadados do nft (dado tokenId)
-async function nftMetadata(tokenId) {
+// Recupera os metadados dos nfts com status = sale
+async function getNftOnSaleMetadata() {
   let token = localStorage.getItem("token");
   let headers = new Headers();
-  headers.append("Content-Type", "application/json");
   headers.append("Authorization", "Bearer " + token);
-
-  let url = `https://${HOST}:${PORT}/meta/getMetadata?tokenId=${tokenId.slice(1)}`;
+  let url = `https://${HOST}:${PORT}/query/channels/mychannel/chaincodes/erc1155/GetNFTsFromStatus?status=sale`;
   var init = {
     method: "GET",
     headers: headers,
   };
 
   let response = await fetch(url, init);
-  return response.json();
+  let result = (await response.json());
+  let nftArray = [];
+  // Retornar array contendo somente a lista de ids dos nfts
+  for (var i in result) {
+    nftArray = nftArray.concat(result[i]);
+  }
+
+  for (var el in nftArray){
+    // Adiciona um _ na frente dos ids para evitar problemas de nomeclatura de ID com HTML4 (Ids iniciando com numeros não sao aceitos)    
+    nftArray[el][0] = "_"+ nftArray[el][0];
+  }
+  return nftArray;
 }
 
 // Retorna string com a construção dos metadados de dado nft (em div accordion colapsavel)
@@ -154,7 +191,7 @@ async function renderMetadata(tokenId, nftinfo) {
       '<div class="accordion-body">' +
         "<p>" +
           `<b> Status: </b> ${nftinfo?.metadata?.status} <br />` +
-          `<b> Quantidade: </b> ${nftinfo?.metadata?.amount} <br />` +
+          `<b> Quantidade: </b> ${nftinfo?.amount} <br />` +
           `<b> Proprietário da Terra: </b> ${nftinfo?.metadata?.land_owner} <br />` +
           `<b> Dono dos direitos de Compensação: </b> ${nftinfo?.metadata?.compensation_owner} <br />` +
           renderCompensation(tokenId.replace(/\s/g, ""), nftinfo?.metadata?.compensation_state) +
@@ -195,6 +232,7 @@ function seeMoreButton(){
   }
 }
 
+// função de compra do nft
 async function buy(tokenIdInput){
   document.getElementById("nft-showroom").style.display = "none";
   document.getElementById("loader").style.display = "flex";
@@ -258,28 +296,3 @@ async function buy(tokenIdInput){
 
 }
 
-// Recupera os nfts do usuario logado
-async function getNftTokens() {
-  let token = localStorage.getItem("token");
-  let headers = new Headers();
-  headers.append("Authorization", "Bearer " + token);
-  let url = `https://${HOST}:${PORT}/query/channels/mychannel/chaincodes/erc1155/GetNFTsFromStatus?status=sale`;
-  var init = {
-    method: "GET",
-    headers: headers,
-  };
-
-  let response = await fetch(url, init);
-  let result = (await response.json());
-  let nftArray = [];
-  // Retornar array contendo somente a lista de ids dos nfts
-  for (var i in result) {
-    nftArray = nftArray.concat(result[i]);
-  }
-
-  for (var el in nftArray){
-    // Adiciona um _ na frente dos ids para evitar problemas de nomeclatura de ID com HTML4 (Ids iniciando com numeros não sao aceitos)    
-    nftArray[el][0] = "_"+ nftArray[el][0];
-  }
-  return nftArray;
-}
