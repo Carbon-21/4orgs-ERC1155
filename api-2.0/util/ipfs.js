1e3261d8d20f55a65a4680370a73ba08f50d4c9e
Pedro <correia.barcha@gmail.com>
2023-08-03 12:19:00 -0300

    ipns implementado

--- api-2.0/util/ipfs.js
+++ api-2.0/util/ipfs.js
@@ -1,28 +1,73 @@
 const logger = require("./logger");
-// import { createHelia } from "helia";
-
-// create a Helia node
 
 let helia;
 
-async function loadIpfs() {
+async function createNode() {
+  const { noise } = await import("@chainsafe/libp2p-noise");
+  const { yamux } = await import("@chainsafe/libp2p-yamux");
+  const { bootstrap } = await import("@libp2p/bootstrap");
+  const { tcp } = await import("@libp2p/tcp");
+  const { MemoryBlockstore } = await import("blockstore-core");
+  const { MemoryDatastore } = await import("datastore-core");
   const { createHelia } = await import("helia");
+  const { createLibp2p } = await import("libp2p");
+  const { identifyService } = await import("libp2p/identify");
 
-  logger.info("Creating IPFS object...");
+  // the blockstore is where we store the blocks that make up files
+  const blockstore = new MemoryBlockstore();
 
-  helia = await createHelia();
+  // application-specific data lives in the datastore
+  const datastore = new MemoryDatastore();
 
-  logger.info("Done. Node Peer ID:", helia.libp2p.peerId);
+  // libp2p is the networking layer that underpins Helia
+  const libp2p = await createLibp2p({
+    datastore,
+    addresses: {
+      // listen: ["/ip4/127.0.0.1/tcp/0"], so localhost
+      listen: ["/ip4/0.0.0.0/tcp/0"],
+    },
+    transports: [tcp()],
+    connectionEncryption: [noise()],
+    streamMuxers: [yamux()],
+    peerDiscovery: [
+      bootstrap({
+        list: [
+          "/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
+          "/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
+          "/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
+          "/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt",
+        ],
+      }),
+    ],
+    services: {
+      identify: identifyService(),
+    },
+    // TODO config UpNP?
+  });
+
+  helia = await createHelia({
+    datastore,
+    blockstore,
+    libp2p,
+  });
+
+  console.log("Peer ID: ", helia.libp2p.peerId);
+  console.log("getPeers: ", helia.libp2p.getPeers());
+  console.log("Multiaddrs:", helia.libp2p.getMultiaddrs());
 }
 
 exports.writeIPFS = async (data) => {
   try {
     const { unixfs } = await import("@helia/unixfs");
 
-    //initialize IPFS if it didn't happen already
-    if (!helia) await loadIpfs();
+    //initialize IPFS node if it didn't happen already
+    if (!helia) await createNode();
 
-    //WRITE
+    // create 2nd node and connect them together
+    // const node2 = await createNode();
+    // const multiaddrs = node2.libp2p.getMultiaddrs();
+    // await helia.libp2p.dial(multiaddrs[0]);
+
     // create a filesystem on top of Helia, in this case it's UnixFS
     const fs = unixfs(helia);
 
@@ -30,113 +75,218 @@ exports.writeIPFS = async (data) => {
     const encoder = new TextEncoder();
 
     // add the bytes to your node and receive a unique content identifier
-    const cid = await fs.addBytes(encoder.encode("Hello World 101"), helia.blockstore);
+    const cid = await fs.addBytes(encoder.encode("jamiroaiiii"));
 
-    logger.info("Added file:", cid.toString());
+    console.log("Added file:", cid.toString());
 
-    //READ
+    ipnsPublish(cid);
+  } catch (error) {
+    logger.error(error);
+  }
+};
+
+const ipnsPublish = async (cid) => {
+  try {
+    const { ipns } = await import("@helia/ipns");
+
+    const name = ipns(helia, [
+      // configure routings here
+    ]);
+
+    // create a public key to publish as an IPNS name
+    const keyInfo = await helia.libp2p.keychain.createKey("my-key", "rsa");
+    console.log("keyInfo", keyInfo);
+    const peerId = await helia.libp2p.keychain.exportPeerId(keyInfo.name);
+    console.log("peerId", peerId);
+
+    // publish the name
+    await name.publish(peerId, cid);
+
+    // resolve the name
+    const resolvedCid = await name.resolve(peerId);
+    console.log("cid retornado do IPNS:", resolvedCid);
+    console.log("agora consultar esse cid...");
+    readIPFS(resolvedCid);
+  } catch (error) {
+    logger.error(error);
+  }
+};
+
+// exports.readIPFS = async (cid) => {
+const readIPFS = async (cid) => {
+  try {
+    const { unixfs } = await import("@helia/unixfs");
+
+    //initialize IPFS node if it didn't happen already
+    if (!helia) await createNode();
+
+    // create a filesystem on top of Helia, in this case it's UnixFS
+    const fs = unixfs(helia);
+
+    // create a filesystem on top of the second Helia node
+    // const fs2 = unixfs(node2);
+
     // this decoder will turn Uint8Arrays into strings
     const decoder = new TextDecoder();
     let text = "";
 
+    // fetch the file from the first Helia node
     for await (const chunk of fs.cat(cid)) {
       text += decoder.decode(chunk, {
         stream: true,
       });
     }
+    console.log("Fetched file contents:", text);
 
-    console.log("Added file contents:", text);
+    //wait (eu que coloquei isso, pra teste, mas acho q n precisa)
+    // await new Promise((resolve) => setTimeout(resolve, 500000));
 
-    //write (not using MFS)
-    // const { cid } = await ipfs.add(JSON.stringify(data, { pin: false }));
-
-    // const timestamp = Date.now();
-    // // await ipfs.files.mkdir(`/${timestamp}`);
-    // // await ipfs.files.mkdir(`/${timestamp}`);
-    // console.log("AA");
-
-    // await ipfs.files.write("/tail.json", "teste", { create: true });
-    // const { cid } = await ipfs.files.stat("/tail.json");
-    // console.log(cid);
-
-    // // let ipfsData = [];
-    // let ipfsData = "";
-    // for await (const chunk of ipfs.files.read(cid)) {
-    //   ipfsData = chunk;
-    //   // ipfsData.push(chunk);
-    // }
-    // // console.log(uint8ArrayConcat(ipfsData).toString());
-    // console.log(ipfsData);
-
-    // console.log("BB");
-    // // const { cid } = await ipfs.files.write("/tail.json", data);
-    // cid = "banana";
-
-    // The address of your files.
-
-    // await ipfs.stop();
-    // await ipfs.start();
-    // const res = await ipfs.name.publish(cid);
-    // logger.info("cabou");
-    // You now have a res which contains two fields:
-    //   - name: the name under which the content was published.
-    //   - value: the "real" address to which Name points.
-    // logger.info(`https://gateway.ipfs.io/ipns/${res.name}`);
-
-    // const addr = `/ipfs/${cid.toString()}`;
-    // logger.info(addr);
-    // const res = await ipfs.name.publish(addr, { allowOffline: true });
-
-    // const { cidd } = await ipfs.add(JSON.stringify({ data: "data" }, { pin: false }));
-    // const addrr = `/ipfs/${cidd.toString()}`;
-    // await ipfs.name.publish(addrr, { allowOffline: true });
-
-    // const res = await ipfs.name.publish(addr, { allowOffline: true, resolve: false, lifetime: "2h" });
-
-    // const res = await ipfs.name.publish(addr);
-    // this.node.name...
-    // logger.info(`https://gateway.ipfs.io/ipns/${res.name}`);
-
-    // return cid;
+    //initialize IPFS node if it didn't happen already
   } catch (error) {
-    logger.error("DEU ERRO EIN", error);
+    logger.error(error);
   }
 };
 
-exports.readIPFS = async (cid) => {
-  try {
-    //initialize IPFS if it didn't happen already
-    if (!helia) await loadIpfs();
+////////////////////HELIA SEM libp2p/////////////////////////
 
-    // //resolver ipns address (i.e. get ipfs address)
-    // const ipnsAddr = "/ipns/12D3KooWP3YLBzYKvQR3fpdfBXLgdj1FX6VGJTDpLg4JAyPJVXZX";
-    // ipfsAddr = "";
-    // for await (const name of ipfs.name.resolve(ipnsAddr)) {
-    //   ipfsAddr = name;
-    //   // an ipfsAddr concat should go here?
-    // }
-    // logger.info(`IPNS addres ${ipnsAddr} points to ${ipfsAddr}`);
+// // import { createHelia } from "helia";
 
-    // const ipfsData = [];
-    // for await (const chunk of ipfs.files.read("/tail.json")) {
-    //   ipfsData.push(chunk);
-    // }
-    // console.log(uint8ArrayConcat(ipfsData).toString());
+// // create a Helia node
 
-    // //read from ipfs (without MFS)
-    // // let ipfsData = "";
-    // // for await (const chunk of ipfs.cat(ipfsAddr)) {
-    // //   ipfsData = Buffer.from(chunk).toString("utf8");
-    // //   // an ipfsData concat should go here?
-    // // }
+// let helia;
 
-    // logger.info("Data retrieved from the IPFS");
+// async function loadIpfs() {
+//   const { createHelia } = await import("helia");
 
-    // return ipfsData;
-  } catch (error) {
-    logger.error("DEU RUIM", error);
-  }
-};
+//   logger.info("Creating IPFS object...");
+
+//   helia = await createHelia();
+
+//   logger.info("Done. Node Peer ID:", helia.libp2p.peerId);
+// }
+
+// exports.writeIPFS = async (data) => {
+//   try {
+//     const { unixfs } = await import("@helia/unixfs");
+
+//     //initialize IPFS if it didn't happen already
+//     if (!helia) await loadIpfs();
+
+//     //WRITE
+//     // create a filesystem on top of Helia, in this case it's UnixFS
+//     const fs = unixfs(helia);
+
+//     // we will use this TextEncoder to turn strings into Uint8Arrays
+//     const encoder = new TextEncoder();
+
+//     // add the bytes to your node and receive a unique content identifier
+//     const cid = await fs.addBytes(encoder.encode("Hello World 101"), helia.blockstore);
+
+//     logger.info("Added file:", cid.toString());
+
+//     //READ
+//     // this decoder will turn Uint8Arrays into strings
+//     const decoder = new TextDecoder();
+//     let text = "";
+
+//     for await (const chunk of fs.cat(cid)) {
+//       text += decoder.decode(chunk, {
+//         stream: true,
+//       });
+//     }
+
+//     console.log("Added file contents:", text);
+
+//write (not using MFS)
+// const { cid } = await ipfs.add(JSON.stringify(data, { pin: false }));
+
+// const timestamp = Date.now();
+// // await ipfs.files.mkdir(`/${timestamp}`);
+// // await ipfs.files.mkdir(`/${timestamp}`);
+// console.log("AA");
+
+// await ipfs.files.write("/tail.json", "teste", { create: true });
+// const { cid } = await ipfs.files.stat("/tail.json");
+// console.log(cid);
+
+// // let ipfsData = [];
+// let ipfsData = "";
+// for await (const chunk of ipfs.files.read(cid)) {
+//   ipfsData = chunk;
+//   // ipfsData.push(chunk);
+// }
+// // console.log(uint8ArrayConcat(ipfsData).toString());
+// console.log(ipfsData);
+
+// console.log("BB");
+// // const { cid } = await ipfs.files.write("/tail.json", data);
+// cid = "banana";
+
+// The address of your files.
+
+// await ipfs.stop();
+// await ipfs.start();
+// const res = await ipfs.name.publish(cid);
+// logger.info("cabou");
+// You now have a res which contains two fields:
+//   - name: the name under which the content was published.
+//   - value: the "real" address to which Name points.
+// logger.info(`https://gateway.ipfs.io/ipns/${res.name}`);
+
+// const addr = `/ipfs/${cid.toString()}`;
+// logger.info(addr);
+// const res = await ipfs.name.publish(addr, { allowOffline: true });
+
+// const { cidd } = await ipfs.add(JSON.stringify({ data: "data" }, { pin: false }));
+// const addrr = `/ipfs/${cidd.toString()}`;
+// await ipfs.name.publish(addrr, { allowOffline: true });
+
+// const res = await ipfs.name.publish(addr, { allowOffline: true, resolve: false, lifetime: "2h" });
+
+// const res = await ipfs.name.publish(addr);
+// this.node.name...
+// logger.info(`https://gateway.ipfs.io/ipns/${res.name}`);
+
+// return cid;
+//   } catch (error) {
+//     logger.error("DEU ERRO EIN", error);
+//   }
+// };
+
+// exports.readIPFS = async (cid) => {
+//   try {
+//     //initialize IPFS if it didn't happen already
+//     if (!helia) await loadIpfs();
+
+// //resolver ipns address (i.e. get ipfs address)
+// const ipnsAddr = "/ipns/12D3KooWP3YLBzYKvQR3fpdfBXLgdj1FX6VGJTDpLg4JAyPJVXZX";
+// ipfsAddr = "";
+// for await (const name of ipfs.name.resolve(ipnsAddr)) {
+//   ipfsAddr = name;
+//   // an ipfsAddr concat should go here?
+// }
+// logger.info(`IPNS addres ${ipnsAddr} points to ${ipfsAddr}`);
+
+// const ipfsData = [];
+// for await (const chunk of ipfs.files.read("/tail.json")) {
+//   ipfsData.push(chunk);
+// }
+// console.log(uint8ArrayConcat(ipfsData).toString());
+
+// //read from ipfs (without MFS)
+// // let ipfsData = "";
+// // for await (const chunk of ipfs.cat(ipfsAddr)) {
+// //   ipfsData = Buffer.from(chunk).toString("utf8");
+// //   // an ipfsData concat should go here?
+// // }
+
+// logger.info("Data retrieved from the IPFS");
+
+// return ipfsData;
+//   } catch (error) {
+//     logger.error("DEU RUIM", error);
+//   }
+// };
 
 ////////// IPFS-CORE ////////////
 // const logger = require("./logger");
