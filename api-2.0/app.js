dd0d7173e806a7f1f75c9011c9a023273eebfef0
Emilly Arcanjo <emilly.ssa@gmail.com>
2023-09-19 23:16:08 -0300

    uncommenting some dependencies

--- api-2.0/app.js
+++ api-2.0/app.js
@@ -19,13 +19,13 @@ const path = require("path");
 const logger = require("./util/logger");
 const cors = require("./middleware/cors");
 const error = require("./middleware/error");
-// const { postTransparencyLog } = require("./controllers/ipfs-controller");
+const { postTransparencyLog } = require("./controllers/ipfs-controller");
 const { createAdmin } = require("./controllers/auth-crontroller");
 
 //routes
 const authRoutes = require("./routes/auth-routes");
-// const ipfsRoutes = require("./routes/ipfs-routes");
-// const invokeRoutes = require("./routes/invoke-routes");
+const ipfsRoutes = require("./routes/ipfs-routes");
+const invokeRoutes = require("./routes/invoke-routes");
 const queryRoutes = require("./routes/query-routes");
 const frontRoutes = require("./routes/front-routes");
 const nftRoutes = require("./routes/nft-routes");
@@ -44,10 +44,10 @@ const app = express();
 //cors
 app.use(cors);
 
-// //timezone
+//timezone
 process.env.TZ = "America/Sao_Paulo";
 
-// //bodyParser
+//bodyParser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -86,16 +86,17 @@ app.get("/", function (req, res) {
   res.render("home", { title: "Home", cssPath: "css/home.css" });
 });
 
-// //transparency log: regularly post blockchain's tail to the IPFS
+//transparency log: regularly post blockchain's tail to the IPFS
 // new cronJob(process.env.LOG_CRONTAB, postTransparencyLog, null, true);
+
 
 ///// ROUTES /////
 app.use("/auth", authRoutes);
-// app.use("/invoke", invokeRoutes);
+app.use("/invoke", invokeRoutes);
 app.use("/query", queryRoutes);
 app.use("/", frontRoutes);
 app.use("/nft", nftRoutes);
-// app.use("/ipfs", ipfsRoutes);
+app.use("/ipfs", ipfsRoutes);
 
 ///// ERROR MIDDLEWARE /////
 //executed if any other middleware yields an error
@@ -103,18 +104,15 @@ app.use(error);
 
 ///// SERVER INIT /////
 //create admin accounts if needed and start the server
-
-// createAdmin()
-//   .then(() => {
-
+createAdmin()
+  .then(() => {
     const httpsServer = https.createServer(options, app);
-    httpsServer.listen(port, host, () => {
+    httpsServer.listen(port, () => {
       logger.info("****************** HTTPS SERVER STARTED ************************");
       logger.info("***************  https://%s:%s  *******************", host, port);
     });
     // postTransparencyLog();
-
-  // })
-  // .catch((err) => {
-  //   logger.fatal("Server couldn't be initialized:", err);
-  // });
+  })
+  .catch((err) => {
+    logger.fatal("Server couldn't be initialized:", err);
+  });
