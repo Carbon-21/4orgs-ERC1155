9032757c9eed5f51f23c97fc34f3df6f4df6ac25
vinisampa <vinisampa26@gmail.com>
2023-10-11 08:31:01 -0300

    fix: nft requests page

--- api-2.0/public/scripts/src/nftRequests.js
+++ api-2.0/public/scripts/src/nftRequests.js
@@ -1,4 +1,4 @@
-window.getRequests = async () => {
+window.getRequests = async (status) => {
     event.preventDefault();
     let token = localStorage.getItem("token");
     console.log(token);
@@ -7,7 +7,7 @@ window.getRequests = async () => {
     headers.append("Authorization", "Bearer " + token);
 
     // trocar para variaveis de host e port
-    let url = 'https://localhost:4000/nft/requests?requestStatus=pending';
+    let url = `https://localhost:4000/nft/requests?requestStatus=${status}`;
 
     var init = {
         method: "GET",
@@ -15,13 +15,60 @@ window.getRequests = async () => {
     };
 
     let response = await fetch(url, init);
-    
-    console.log(response);
-    console.log(response.body);
-    console.log(response.json());
+
+    console.log(response)
 
     if (response.ok) {
         return await response.json();
+    } else {
+        console.log("HTTP Error ", response.status);
+        return null;
+    }
+}
+
+window.requests = async () => {
+    event.preventDefault();
+    const status = document.getElementById("statusDropDown").value;
+    const search = document.getElementById("searchInput").value;
+    console.log(status, search);
+    // Recuperar todos os requests na variável requestsList
+    let {requests: requestsList} = await getRequests(status);
+    
+    // Caso haja requests
+    if (requestsList) {
+        console.log('list', requestsList)
+        let element = '';
+        const requestsFiltered = requestsList.filter((fil)=>{
+            return (
+                fil.landOwner.toLowerCase().includes(search.toLowerCase()) ||
+                fil.phyto.toLowerCase().includes(search.toLowerCase()) ||
+                fil.landArea.toLowerCase().includes(search.toLowerCase())
+            )
+        });
+        if (requestsFiltered.length === 0){
+            element +=
+                `<div class="d-flex flex-column justify-content-between p-md-1">
+                    <center><h2><font color="#5f5f5f">Sem solicitações no momento </font></h2> </center>
+                </div>`;
+            document.getElementById("requests-show").innerHTML = element;        
+        } else {
+            requestsFiltered.map((req) => {
+                element +=
+                    `<a class="request-data" href="/nft/mint?requestId=${req.id}">
+                        <div class="info-data">
+                            <p><b>Dono da terra: </b> ${req.landOwner}</p>
+                        </div>
+                        <div class="info-data">
+                            <p><b>Fitofisiologia: </b> ${req.phyto}</p>
+                            <div class="info-data">
+                                <p><b>Área da terra: </b> ${req.landArea} hectares</p>
+                            </div>
+                        </div>
+                    </a>`;
+            })
+            // Renderizar a cada request carregado
+            document.getElementById("requests-show").innerHTML = element;
+        }
     } else {
         console.log("HTTP Error ", response.status);
         return null;
