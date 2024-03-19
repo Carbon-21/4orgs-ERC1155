cf652ac56213c24c2b7989c51a808de28063f3f8
leticiafalconer <leticia@falconer.com.br>
2024-03-19 15:58:58 -0300

    test

--- api-2.0/config/genccp-docker.sh
+++ api-2.0/config/genccp-docker.sh
@@ -16,26 +16,26 @@ function json_ccp {
         -e "s/\${CAPORT}/$3/" \
         -e "s#\${PEERPEM}#$PP#" \
         -e "s#\${CAPEM}#$CP#" \
-        ./ccp-template.json
+        ./apache2-ccp-template.json
 }
 
 ORG=carbon
 P0PORT=7051
 CAPORT=7054
-PEERPEM=../artifacts/channel/crypto-config/peerOrganizations/carbon.example.com/peers/peer0.carbon.example.com/tls/tlscacerts/tls-localhost-7054-ca-carbon-example-com.pem
-CAPEM=../artifacts/channel/crypto-config/peerOrganizations/carbon.example.com/msp/tlscacerts/ca.crt
+PEERPEM=../../../artifacts/channel/crypto-config/peerOrganizations/carbon.example.com/peers/peer0.carbon.example.com/tls/tlscacerts/tls-localhost-7054-ca-carbon-example-com.pem
+CAPEM=../../../artifacts/channel/crypto-config/peerOrganizations/carbon.example.com/msp/tlscacerts/ca.crt
 echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM $HOST)" > connection-carbon.json
 
 ORG=cetesb
 P0PORT=11051
 CAPORT=10054
-PEERPEM=../artifacts/channel/crypto-config/peerOrganizations/cetesb.example.com/peers/peer0.cetesb.example.com/tls/tlscacerts/tls-localhost-10054-ca-cetesb-example-com.pem
-CAPEM=../artifacts/channel/crypto-config/peerOrganizations/cetesb.example.com/msp/tlscacerts/ca.crt
+PEERPEM=../../../artifacts/channel/crypto-config/peerOrganizations/cetesb.example.com/peers/peer0.cetesb.example.com/tls/tlscacerts/tls-localhost-10054-ca-cetesb-example-com.pem
+CAPEM=../../../artifacts/channel/crypto-config/peerOrganizations/cetesb.example.com/msp/tlscacerts/ca.crt
 echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM $HOST)" > connection-cetesb.json
 
 ORG=ibama
 P0PORT=13051
 CAPORT=11054
-PEERPEM=../artifacts/channel/crypto-config/peerOrganizations/ibama.example.com/peers/peer0.ibama.example.com/tls/tlscacerts/tls-localhost-11054-ca-ibama-example-com.pem
-CAPEM=../artifacts/channel/crypto-config/peerOrganizations/ibama.example.com/msp/tlscacerts/ca.crt
+PEERPEM=../../../artifacts/channel/crypto-config/peerOrganizations/ibama.example.com/peers/peer0.ibama.example.com/tls/tlscacerts/tls-localhost-11054-ca-ibama-example-com.pem
+CAPEM=../../../artifacts/channel/crypto-config/peerOrganizations/ibama.example.com/msp/tlscacerts/ca.crt
 echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM $HOST)" > connection-ibama.json
--- api-2.0/public/scripts/src/genscripts.sh
+++ api-2.0/public/scripts/src/genscripts.sh
@@ -1,11 +1,13 @@
 
 #!/bin/bash
 cd "$(dirname "$0")"
+echo "entrou genscripts"
 source ../../../.env
+
 
 function update() {
 
-    for file in *.js ; do
+    for file in ../../../public/scripts/src/*.js ; do
         echo "Updating host and port for: ${file}"
         sed -e "s/\${HOST}/$1/g" \
             -e "s/\${PORT}/$2/g"  \
